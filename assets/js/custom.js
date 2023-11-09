b58c87e783575c12c38420f8a63784d94e346c45
Sreerag Pavithran <sreeragpavithran@gmail.com>
2023-11-09 12:26:40 +0530

    Changed Loader

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -494,19 +494,19 @@ lightmodeToggle.addEventListener("click", () => {
 });
 
 // Preloader
-$(window).on("load", function () {
-  $("body").addClass("page-loaded");
-  lightmodeToggle.addEventListener("click", () => {
-    lightmode = localStorage.getItem("light-d");
+// $(window).on("load", function () {
+//   $("body").addClass("page-loaded");
+//   lightmodeToggle.addEventListener("click", () => {
+//     lightmode = localStorage.getItem("light-d");
 
-    if (lightmode !== "enabled") {
-      disablelightmode();
-    } else {
-      disablelightmode();
-    }
-  });
-  console.log("loaded");
-});
+//     if (lightmode !== "enabled") {
+//       disablelightmode();
+//     } else {
+//       disablelightmode();
+//     }
+//   });
+//   console.log("loaded");
+// });
 
 // document.addEventListener('contextmenu', function(e) {
 //      e.preventDefault();
